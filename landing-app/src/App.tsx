c3c4ae37c69a8df880c3eee869467bc6c03a52b2
Wave780 <jonahemma12@gmail.com>
2026-02-13 16:45:10 +0100

    fix update

--- landing-app/src/App.tsx
+++ landing-app/src/App.tsx
@@ -1,28 +1,48 @@
 import React from 'react'
 import './App.css'
 import './index.css'
-import Card from './Card';
+import Card from './components/Card';
+
+interface ICard {
+  title: string;
+  description: string;
+}
 
 const  App: React.FC = () => {
+
+  const cards: ICard[] = [
+    {
+      title: "MISSION",
+      description: "To empower individuals and organizations to unlock their full potential by providing innovative, creative, and tailored solutions that inspire growth, foster innovation, and shape a brighter future"
+    },
+    {
+      title: "VISION",
+      description: "To empower individuals and organizations to unlock their full potential by providing innovative, creative, and tailored solutions that inspire growth, foster innovation, and shape a brighter future"
+    },
+    {
+      title: "VALUES",
+      description: "To empower individuals and organizations to unlock their full potential by providing innovative, creative, and tailored solutions that inspire growth, foster innovation, and shape a brighter future"
+    }
+  ];
+
+
+  const navItems: { name: string; href: string }[] = [
+    { name: 'Home', href: '#home' },
+    { name: 'About', href: '#about' },
+    { name: 'Services', href: '#services' },
+    { name: 'Contact', href: '#contact' },
+  ];
+
   return(
     <>
        <header>
         <nav className='nav'>
-        <li>
-          <a className='nav-link' href="#home">Home</a>
-        </li>
-        <li>
-        <a className='nav-link' href="#about">About</a>
-        </li>
-        <li>
-
-        <a className='nav-link' href="#services">Services</a>
-        </li>
-        <li>
-
-        <a className='nav-link' href="#contact">Contact</a>
-        </li>
-      </nav>
+          {navItems.map((item) => (
+            <li key={item.name}>
+              <a className='nav-link' href={item.href}>{item.name}</a>
+            </li>
+          ))}
+        </nav>
       </header>
     <div className='div'>
 
@@ -39,23 +59,16 @@ const  App: React.FC = () => {
         </div>
        
        
-       
     </div>
         <div className="card-container">
-      <Card 
-      title="MISSION" 
-        description="To empower individuals and organizations to unlock their full potential by providing innovative, creative, and tailored solutions that inspire growth, foster innovation, and shape a brighter future" 
-      />
-      <Card 
-        title="VISION" 
-        description="To be a global leader in transforming ideas into impactful realities, creating a world where everyone has the tools and confidence to design their own future" 
-      />
-      <Card 
-        title="VALUES" 
-        description="Fostering partnerships and teamwork to achieve shared success through collaboration and mutual support" 
-      />
+          {cards.map(card => (
+             <Card 
+                key={card.title}
+                title={card.title} 
+                description={card.description} 
+              />
+          ))}
         </div>
-        
     </>
       )
 }
